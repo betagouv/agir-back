084a37c9a20bd56f083d293acad3919e37c93cb1
Wojciech WOJCIK <wwojcik@octo.com>
2023-06-09 10:36:37 +0200

    ajout d'annotations pour une meilleur doc API

--- src/infrastructure/api/citoyen.controller.ts
+++ src/infrastructure/api/citoyen.controller.ts
@@ -1,7 +1,7 @@
 import { Citoyen } from '.prisma/client';
 import { Body, Controller, Get, NotFoundException, Param, Post } from '@nestjs/common';
 import { CitoyenUsecase } from '../../usecase/citoyen.usecase';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiBody } from '@nestjs/swagger';
 
 @Controller()
 @ApiTags('Citoyen')
@@ -9,7 +9,7 @@ export class CitoyenController {
   constructor(private readonly citoyenUsecase: CitoyenUsecase) {}
 
   @Get('citoyens/:id')
-  async getCitoyenName(@Param('id') id): Promise<Citoyen> {
+  async getCitoyenName(@Param('id') id:number): Promise<Citoyen> {
     const citoyen = await this.citoyenUsecase.getCitoyen(Number(id));
     if (citoyen == null) {
       throw new NotFoundException(`Pas de citoyen d'id ${id}`);
@@ -21,7 +21,16 @@ export class CitoyenController {
     return await this.citoyenUsecase.listCitoyens();
   }
   @Post('citoyens')
-  async createCitoyen(@Body() body): Promise<Citoyen> {
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        name: {type : "string"},
+        conso: {type : "integer"},
+      },
+    },
+  })
+  async createCitoyen(@Body() body:Citoyen): Promise<Citoyen> {
     return this.citoyenUsecase.createCitoyen(body.name, body.conso);
   }
 }
