1a8beabf0f45af7fd7f1f267f0b031838c9e3779
Wojciech WOJCIK <wwojcik@octo.com>
2023-06-12 09:10:46 +0200

    'tryitout' directement accessible sur la page swaggerui

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,23 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder, SwaggerCustomOptions } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Exposition Swagger
-  const config = new DocumentBuilder()
+  const swaggerDocumentOptions: SwaggerCustomOptions =  {
+    swaggerOptions: {
+      tryItOutEnabled: true,
+    }
+  };
+    const config = new DocumentBuilder()
     .setTitle('Agir back')
     .setDescription('Doc API Agir')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, swaggerDocumentOptions);
 
   // Activation des requetes cross origin pour le front
   app.enableCors();
