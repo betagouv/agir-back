ed84aa501364c92bc4edc0aed9a363d77d61d860
Wojciech WOJCIK <wwojcik@octo.com>
2023-07-11 17:11:02 +0200

    clean up

--- test_data/interactions/suivis.ts
+++ test_data/interactions/suivis.ts
@@ -1,27 +1,16 @@
 const suivis = {
-  suivi_alimentation: {
-    titre: "Comment avez vous mangé ajd'hui ?",
+  suivi_du_jour: {
+    titre: 'Faire le suivi du jour',
     soustitre: 'bla bla',
-    categorie: 'suivi',
+    categorie: 'Global',
     tags: ['suivi', 'alimentation', 'kyc'],
     duree: '⏱️ < 1 minute',
-    frequence: '1x/jour',
-    image_url: 'https://picsum.photos/200/300',
+    frequence: null,
+    image_url:
+      'https://images.unsplash.com/photo-1521079299535-94854b0a7b27?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1748&q=80',
     url: null,
     difficulty: 1,
-    points: 20,
-  },
-  suivi_transport: {
-    titre: "Passer à l'électrique",
-    soustitre: 'bla bla',
-    categorie: 'Voiture',
-    tags: ['suivi', 'transport', 'kyc'],
-    duree: '⏱️ < 2 minute',
-    frequence: '1x/jour',
-    image_url: 'https://picsum.photos/200/300',
-    url: null,
-    difficulty: 2,
-    points: 20,
+    points: 25,
   },
 };
 module.exports = suivis;
--- test_data/utilisateurs_content.ts
+++ test_data/utilisateurs_content.ts
@@ -5,7 +5,7 @@ const utilisateurs = {
     interactions: [
       { id: 'article_eau', reco_score: 5 },
       { id: 'article_viande', reco_score: 10 },
-      { id: 'suivi_alimentation', reco_score: 1 },
+      { id: 'suivi_du_jour', reco_score: 1 },
       { id: 'tout_sur_leau', reco_score: 2 },
     ],
     suivis: ['sa1', 'sa2', 'st1'],
@@ -16,8 +16,7 @@ const utilisateurs = {
     interactions: [
       { id: 'aide_velo', reco_score: 4 },
       { id: 'aide_retrofit', reco_score: 15 },
-      { id: 'suivi_alimentation', reco_score: 1 },
-      { id: 'suivi_transport', reco_score: 2 },
+      { id: 'suivi_du_jour', reco_score: 1 },
       { id: 'tout_sur_leau', reco_score: 3 },
       { id: 'poisson', reco_score: 10 },
     ],
