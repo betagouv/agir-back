15e6bfa8fa4c880bd842e7410f8dd883b148e49e
Wojciech WOJCIK <wwojcik@octo.com>
2023-07-17 08:51:43 +0200

    clean up

---
+++ test_data/evenements/badges.ts
@@ -0,0 +1,11 @@
+const badges = {
+  badge1: {
+    date: '2023-02-17',
+    titre: 'Premier badge !',
+  },
+  badge2: {
+    date: '2023-03-17',
+    titre: 'Second badge o/',
+  },
+};
+module.exports = badges;
