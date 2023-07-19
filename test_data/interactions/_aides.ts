535f1092d572f7942b15a485543367809e9d94d9
Benoit CHAMPY <benoitchampy@gmail.com>
2023-07-19 18:10:58 +0200

    Jeu de données de test - Aides vélo dans le bon fichier

--- test_data/interactions/_aides.ts
+++ test_data/interactions/_aides.ts
@@ -1,15 +1,15 @@
 const aides = {
   aide_velo: {
-    titre: 'Velo pas cher !',
-    soustitre: 'bla bla',
-    categorie: 'Vélo',
-    tags: ['aide', 'mobilité', 'vélo'],
-    duree: '⏱️ < 1 minute',
+    titre: 'Simulez une aide pour acheter un vélo',
+    soustitre: '',
+    categorie: 'Transport',
+    tags: [],
+    duree: '⏱️ 2 minute',
     frequence: 'Une fois',
-    image_url: 'https://picsum.photos/200/300',
+    image_url: 'https://images.unsplash.com/photo-1570679334008-c97544c8695b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGJpa2V8ZW58MHx8MHx8fDI%3D&auto=format&fit=crop&w=800&q=60',
     url: null,
     difficulty: 1,
-    points: 20,
+    points: 25,
     locked: false,
   },
   aide_retrofit: {
