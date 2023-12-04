0a59c625388df2f868c409d0f6cb0c85d0a885dd
Wojciech WOJCIK <wwojcik@octo.com>
2023-12-04 12:17:20 +0100

    retour de pointage spécifique des articles pour ww@w.com

--- test_data/utilisateurs_content.ts
+++ test_data/utilisateurs_content.ts
@@ -131,7 +131,7 @@ const utilisateurs = {
     gamification: {
       points: 0,
     },
-    //interactions: ALL_INTERACTIONS,
+    interactions: ALL_INTERACTIONS,
     suivis: ['sa1', 'sa2', 'sa3', 'st1', 'st2', 'st3'],
     bilans: ['bilan1'],
     badges: ['badge1', 'badge2'],
