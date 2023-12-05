1166dba9c04c4ac4ca7b98870ec3b36365a7d630
Denis B <denis.baudot@gmail.com>
2023-12-05 14:36:27 +0100

    add sentry

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import {
   SwaggerModule,
   DocumentBuilder,
   SwaggerCustomOptions,
 } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as Sentry from '@sentry/node';
+import { SentryFilter } from './infrastructure/sentry.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -23,6 +25,14 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document, swaggerDocumentOptions);
+
+  // Activation de Sentry
+  Sentry.init({
+    dsn: process.env.SENTRY_DNS,
+    tracesSampleRate: 1.0,
+  });
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new SentryFilter(httpAdapter));
 
   // Activation des requetes cross origin pour le front
   app.enableCors();
