569aff6ebae3b68362e6bc8a5435aa949b7e2f87
Wojciech WOJCIK <wwojcik@octo.com>
2024-03-11 10:23:17 +0100

    clean up

--- src/usecase/recommandation.usecase.ts
+++ src/usecase/recommandation.usecase.ts
@@ -32,8 +32,6 @@ export class RecommandationUsecase {
       defis = await this.getDefis(utilisateur);
     }
 
-    console.log(defis);
-
     result.push(...articles);
     result.push(...quizzes);
     result.push(...defis);
