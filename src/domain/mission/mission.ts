177910317abd3c01bc8d95fcf71732e55ac38d7f
Wojciech WOJCIK <wwojcik@octo.com>
2024-07-01 14:40:47 +0200

    pas de points en double pour un défi

--- src/domain/mission/mission.ts
+++ src/domain/mission/mission.ts
@@ -142,10 +142,6 @@ export class Mission {
 
     if (objectif && !objectif.isDone()) {
       objectif.done_at = new Date();
-      utilisateur.gamification.ajoutePoints(
-        objectif.points,
-        utilisateur.unlocked_features,
-      );
       return this.terminerMission(utilisateur);
     }
     return [];
