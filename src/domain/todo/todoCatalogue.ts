a4ee1d89933028a5d5d03b3054924cbe65826bae
Wojciech WOJCIK <wwojcik@octo.com>
2024-09-24 16:34:36 +0200

    todo de test temp

--- src/domain/todo/todoCatalogue.ts
+++ src/domain/todo/todoCatalogue.ts
@@ -183,7 +183,7 @@ export class TodoCatalogue {
         {
           id: uuidv4(),
           titre: `5 questions pour avoir une première estimation de son empreinte`,
-          content_id: 'ENCHAINEMENT_KYC_mini_bilan_carbone',
+          content_id: 'ENCHAINEMENT_KYC_1',
           thematiques: [Thematique.climat],
           progression: { current: 0, target: 1 },
           sont_points_en_poche: false,
