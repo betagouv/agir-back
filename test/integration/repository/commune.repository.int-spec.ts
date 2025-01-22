2192a864a0dd07a9e1eba506ccb54d5b6c4a4e79
Wojciech WOJCIK <wojciech.wojcik@gmail.com>
2025-01-22 09:49:32 +0100

    fonction lister commune par EPCI

--- test/integration/repository/commune.repository.int-spec.ts
+++ test/integration/repository/commune.repository.int-spec.ts
@@ -18,32 +18,28 @@ describe('CommuneRepository', () => {
 
   it('checkCodePostal : revoie true si le code postal existe', async () => {
     // WHEN
-    const result = await communeRepository.checkCodePostal('91120');
+    const result = communeRepository.checkCodePostal('91120');
 
     // THEN
     expect(result).toStrictEqual(true);
   });
   it('checkCodePostal : revoie false si le code postal non existant', async () => {
     // WHEN
-    const result = await communeRepository.checkCodePostal('99999');
+    const result = communeRepository.checkCodePostal('99999');
 
     // THEN
     expect(result).toStrictEqual(false);
   });
   it('getListCommunesParCodePostal : revoie liste vide si le code postal non existant', async () => {
     // WHEN
-    const result = await communeRepository.getListCommunesParCodePostal(
-      '99999',
-    );
+    const result = communeRepository.getListCommunesParCodePostal('99999');
 
     // THEN
     expect(result).toHaveLength(0);
   });
   it('getListCommunesParCodePostal : revoie bonne liste si le code postal ok', async () => {
     // WHEN
-    const result = await communeRepository.getListCommunesParCodePostal(
-      '26290',
-    );
+    const result = communeRepository.getListCommunesParCodePostal('26290');
 
     // THEN
     expect(result).toHaveLength(2);
@@ -77,9 +73,7 @@ describe('CommuneRepository', () => {
   });
   it('getListCommunesParCodePostal : supprime les doublons du fichier chargé', async () => {
     // WHEN
-    const result = await communeRepository.getListCommunesParCodePostal(
-      '10120',
-    );
+    const result = communeRepository.getListCommunesParCodePostal('10120');
 
     // THEN
     expect(result).toHaveLength(4); // au lieu de 6 à cause de lieux dits
@@ -87,54 +81,42 @@ describe('CommuneRepository', () => {
 
   it('findDepartementRegionByCodePostal : renvoie 91 pour 91120', async () => {
     // WHEN
-    const result = await communeRepository.findDepartementRegionByCodePostal(
-      '91120',
-    );
+    const result = communeRepository.findDepartementRegionByCodePostal('91120');
 
     // THEN
     expect(result).toEqual({ code_departement: '91', code_region: '11' });
   });
   it('findDepartementRegionByCodePostal : renvoie 1 pour 01500', async () => {
     // WHEN
-    const result = await communeRepository.findDepartementRegionByCodePostal(
-      '01500',
-    );
+    const result = communeRepository.findDepartementRegionByCodePostal('01500');
 
     // THEN
     expect(result).toEqual({ code_departement: '01', code_region: '84' });
   });
   it('findDepartementRegionByCodePostal : renvoie 2A pour 20000 (Ajaccio)', async () => {
     // WHEN
-    const result = await communeRepository.findDepartementRegionByCodePostal(
-      '20000',
-    );
+    const result = communeRepository.findDepartementRegionByCodePostal('20000');
 
     // THEN
     expect(result).toEqual({ code_departement: '2A', code_region: '94' });
   });
   it('findDepartementRegionByCodePostal : renvoie ok pour le premier arrondissement de Lyon', async () => {
     // WHEN
-    const result = await communeRepository.findDepartementRegionByCodePostal(
-      '69001',
-    );
+    const result = communeRepository.findDepartementRegionByCodePostal('69001');
 
     // THEN
     expect(result).toEqual({ code_departement: '69', code_region: '84' });
   });
   it('findDepartementRegionByCodePostal : renvoie ok pour le premier arrondissement de Paris', async () => {
     // WHEN
-    const result = await communeRepository.findDepartementRegionByCodePostal(
-      '75002',
-    );
+    const result = communeRepository.findDepartementRegionByCodePostal('75002');
 
     // THEN
     expect(result).toEqual({ code_departement: '75', code_region: '11' });
   });
   it('findDepartementRegionByCodePostal : renvoie 2B pour 20287 (Meria)', async () => {
     // WHEN
-    const result = await communeRepository.findDepartementRegionByCodePostal(
-      '20287',
-    );
+    const result = communeRepository.findDepartementRegionByCodePostal('20287');
 
     // THEN
     expect(result).toEqual({
@@ -145,7 +127,7 @@ describe('CommuneRepository', () => {
   it('findRaisonSocialeDeNatureJuridiqueByCodePostal : renvoi la metropole quand ça match', async () => {
     // WHEN
     const result =
-      await communeRepository.findRaisonSocialeDeNatureJuridiqueByCodePostal(
+      communeRepository.findRaisonSocialeDeNatureJuridiqueByCodePostal(
         '29280',
         'METRO',
       );
@@ -156,7 +138,7 @@ describe('CommuneRepository', () => {
   it('findRaisonSocialeDeNatureJuridiqueByCodePostal : liste vide si pas de match metropole', async () => {
     // WHEN
     const result =
-      await communeRepository.findRaisonSocialeDeNatureJuridiqueByCodePostal(
+      communeRepository.findRaisonSocialeDeNatureJuridiqueByCodePostal(
         '77650',
         'METRO',
       );
@@ -167,7 +149,7 @@ describe('CommuneRepository', () => {
   it('findRaisonSocialeDeNatureJuridiqueByCodePostal : renvoi la CA quand ça match', async () => {
     // WHEN
     const result =
-      await communeRepository.findRaisonSocialeDeNatureJuridiqueByCodePostal(
+      communeRepository.findRaisonSocialeDeNatureJuridiqueByCodePostal(
         '01100',
         'CA',
       );
@@ -178,7 +160,7 @@ describe('CommuneRepository', () => {
   it('findRaisonSocialeDeNatureJuridiqueByCodePostal : liste vide si pas de match CA', async () => {
     // WHEN
     const result =
-      await communeRepository.findRaisonSocialeDeNatureJuridiqueByCodePostal(
+      communeRepository.findRaisonSocialeDeNatureJuridiqueByCodePostal(
         '01260',
         'CA',
       );
@@ -189,7 +171,7 @@ describe('CommuneRepository', () => {
   it('findRaisonSocialeDeNatureJuridiqueByCodePostal : renvoi la CC quand ça match', async () => {
     // WHEN
     const result =
-      await communeRepository.findRaisonSocialeDeNatureJuridiqueByCodePostal(
+      communeRepository.findRaisonSocialeDeNatureJuridiqueByCodePostal(
         '01300',
         'CC',
       );
@@ -200,12 +182,21 @@ describe('CommuneRepository', () => {
   it('findRaisonSocialeDeNatureJuridiqueByCodePostal : liste vide si pas de match CC', async () => {
     // WHEN
     const result =
-      await communeRepository.findRaisonSocialeDeNatureJuridiqueByCodePostal(
+      communeRepository.findRaisonSocialeDeNatureJuridiqueByCodePostal(
         '01170',
         'CC',
       );
 
     // THEN
     expect(result).toEqual([]);
+  });
+  it(`getListeCodesCommuneParCodeEPCI : listes communes d'une EPCI`, async () => {
+    // WHEN
+    const result =
+      communeRepository.getListeCodesCommuneParCodeEPCI('242100410');
+
+    // THEN
+    expect(result).toHaveLength(23);
+    expect(result).toContain('21605');
   });
 });
