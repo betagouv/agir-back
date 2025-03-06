5bce84bc899fbf1f7465171ee0a4ccf129633a63
Wojciech WOJCIK <wojciech.wojcik@gmail.com>
2025-03-06 15:04:08 +0100

    clean up

--- src/infrastructure/api/types/utilisateur/codeStateAPI.ts
+++ src/infrastructure/api/types/utilisateur/codeStateAPI.ts
@@ -2,12 +2,12 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class CodeStateAPI {
   @ApiProperty({
-    description: `code OIDC pour finalisation connexion/inscription`,
+    description: `Code OIDC pour finalisation connexion/inscription`,
   })
   oidc_code: string;
 
   @ApiProperty({
-    description: `token technique pour protéger le flux de connexion`,
+    description: `Token technique pour protéger le flux de connexion`,
   })
   oidc_state: string;
 }
