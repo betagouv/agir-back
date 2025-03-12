7ff9b2394468b0d6570ffded94e83bf0091c2de7
Wojciech WOJCIK <wojciech.wojcik@gmail.com>
2025-03-12 11:50:27 +0100

    correction typo tag recette

--- src/infrastructure/repository/services_recherche/recettes/recettes.repository.ts
+++ src/infrastructure/repository/services_recherche/recettes/recettes.repository.ts
@@ -185,7 +185,7 @@ export class RecettesRepository implements FinderInterface {
         new ResultatRecherche({
           id: '' + r.id,
           titre: r.name,
-          difficulty_plat: r.express === 1 ? 'Facile' : 'Intérmédiaire',
+          difficulty_plat: r.express === 1 ? 'Facile' : 'Intermédiaire',
           type_plat: this.mapCategoryPlat(r.recipe_category),
           temps_prepa_min: r.preparation_time,
           image_url: this.getImageUrlFromCategorieRecette(r.recipe_category),
@@ -297,105 +297,4 @@ export class RecettesRepository implements FinderInterface {
     if (cat.includes('PLC')) return 'Plat complet';
     return '-';
   }
-
-  /*
-export type RecettesResponse = {
-  id: string;
-  titre: string;
-  type: string;
-  difficulty: string;
-  temps_prepa_min: number;
-  vege: boolean;
-  vegan: boolean;
-  volaille: boolean;
-  saison: boolean;
-  image_url: string;
-}[];
-*/
-
-  /*
-  private async callServiceAPI(
-    filtre: FiltreRecherche,
-  ): Promise<RecettesResponse> {
-    const result: RecettesResponse = [
-      {
-        id: '1',
-        titre: 'Tian de sardines',
-        difficulty: 'Intérmédiaire',
-        saison: true,
-        temps_prepa_min: 30,
-        type: 'Plat principal',
-        vegan: false,
-        vege: false,
-        volaille: false,
-        image_url:
-          'https://www.mangerbouger.fr/manger-mieux/la-fabrique-a-menus/_next/image?url=https%3A%2F%2Fapi-prod-fam.mangerbouger.fr%2Fstorage%2Frecettes%2Ftian-de-sardines.jpg&w=3840&q=75',
-      },
-      {
-        id: '2',
-        titre: 'Dinde à la provençale',
-        difficulty: 'Facile',
-        saison: true,
-        temps_prepa_min: 40,
-        type: 'Plat principal',
-        vegan: false,
-        vege: false,
-        volaille: true,
-        image_url:
-          'https://www.mangerbouger.fr/manger-mieux/la-fabrique-a-menus/_next/image?url=https%3A%2F%2Fapi-prod-fam.mangerbouger.fr%2Fstorage%2Frecettes%2Fshutterstock_1938638506-dinde-provencale.jpg&w=3840&q=75',
-      },
-      {
-        id: '3',
-        titre: 'Salade crevettes au curry',
-        difficulty: 'Facile',
-        saison: true,
-        temps_prepa_min: 20,
-        type: 'Entrée',
-        vegan: false,
-        vege: false,
-        volaille: false,
-        image_url:
-          'https://www.mangerbouger.fr/manger-mieux/la-fabrique-a-menus/_next/image?url=https%3A%2F%2Fapi-prod-fam.mangerbouger.fr%2Fstorage%2Frecettes%2Fshutterstock_1169506885-salade-crevettes-curry.jpg&w=3840&q=75',
-      },
-      {
-        id: '4',
-        titre: 'Quiche saumon',
-        difficulty: 'Facile',
-        saison: true,
-        temps_prepa_min: 25,
-        type: 'Entrée',
-        vegan: false,
-        vege: false,
-        volaille: false,
-        image_url:
-          'https://www.mangerbouger.fr/manger-mieux/la-fabrique-a-menus/_next/image?url=https%3A%2F%2Fapi-prod-fam.mangerbouger.fr%2Fstorage%2Frecettes%2Fquiche-chou-saumon-et-salade.jpg&w=3840&q=75',
-      },
-      {
-        id: '5',
-        titre: 'Tiramissu',
-        difficulty: 'Facile',
-        saison: true,
-        temps_prepa_min: 20,
-        type: 'Déssert',
-        vegan: false,
-        vege: true,
-        volaille: false,
-        image_url:
-          'https://www.mangerbouger.fr/manger-mieux/la-fabrique-a-menus/_next/image?url=https%3A%2F%2Fapi-prod-fam.mangerbouger.fr%2Fstorage%2Frecettes%2Ftiramisu-aux-fruits-rouges.jpg&w=3840&q=75',
-      },
-    ];
-    switch (filtre.categorie) {
-      case CategorieRecherche.vegan:
-        return [];
-      case CategorieRecherche.vege:
-        return result.filter((a) => a.vege);
-      case CategorieRecherche.dinde_volaille:
-        return result.filter((a) => a.volaille);
-      case CategorieRecherche.saison:
-        return result;
-      default:
-        return result;
-    }
-  }
-  */
 }
