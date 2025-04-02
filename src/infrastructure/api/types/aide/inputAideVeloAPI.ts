0f157c1c48cab1aa23951953083f4b2ffa8f5fae
Emile Rolley <emile.rolley@tuta.io>
2025-04-02 16:15:36 +0200

    feat(aide_velo): ajout du paramètre situation_handicap

--- src/infrastructure/api/types/aide/inputAideVeloAPI.ts
+++ src/infrastructure/api/types/aide/inputAideVeloAPI.ts
@@ -1,11 +1,25 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class InputAideVeloAPI {
-  @ApiProperty() prix_du_velo: number;
   @ApiProperty({
+    description: "Prix d'achat du vélo",
+    type: 'number',
+    required: true,
+  })
+  prix_du_velo: number;
+
+  @ApiProperty({
+    description: 'État du vélo : neuf ou occasion',
     enum: ['neuf', 'occasion'],
     default: 'neuf',
     required: false,
   })
   etat_du_velo: 'neuf' | 'occasion' = 'neuf';
+
+  @ApiProperty({
+    description: "Est-ce que l'utilisateur est en situation de handicap ?",
+    default: false,
+    required: false,
+  })
+  situation_handicap: boolean = false;
 }
