09b771b0f61bff5848d1b49633b007571c8e4c4a
Emile Rolley <emile.rolley@tuta.io>
2025-04-08 19:19:51 +0200

    feat: début nouvelle route POST Mes Aides Réno

--- src/infrastructure/api/mesAidesReno.controller.ts
+++ src/infrastructure/api/mesAidesReno.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Get, Param, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import {
   ApiBearerAuth,
+  ApiBody,
   ApiOkResponse,
   ApiOperation,
   ApiTags,
@@ -28,11 +37,33 @@ export class MesAidesRenoController extends GenericControler {
     description:
       "L'url de l'iframe avec les réponses pré-remplies par rapport à la situation de l'utilisateurice",
   })
-  async faireAction(
+  async getIframeUrl(
     @Param('utilisateurId') utilisateurId: string,
     @Request() req,
   ): Promise<string> {
     this.checkCallerId(req, utilisateurId);
     return await this.mesAidesRenoUsecase.getIframeUrl(utilisateurId);
+  }
+
+  @Post('utilisateurs/:utilisateurId/mes_aides_reno/nouvelles_situation')
+  @UseGuards(AuthGuard)
+  @ApiOperation({
+    summary:
+      "Met à jour la situation de l'utilisateurice avec celle retournée par Mes Aides Réno",
+  })
+  @ApiBody({
+    type: [Object],
+    description: 'La nouvelle situation de l utilisateurice',
+  })
+  async setSituation(
+    @Param('utilisateurId') utilisateurId: string,
+    @Body() nouvellesSituations: Record<string, string>,
+    @Request() req,
+  ): Promise<void> {
+    this.checkCallerId(req, utilisateurId);
+    await this.mesAidesRenoUsecase.updateUtilisateurWith(
+      utilisateurId,
+      nouvellesSituations,
+    );
   }
 }
