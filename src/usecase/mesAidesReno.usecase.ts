3de8539b706abadbc7939d915337a5e50110658b
Emile Rolley <emile.rolley@tuta.io>
2025-04-08 19:19:51 +0200

    feat: début nouvelle route POST Mes Aides Réno

--- src/usecase/mesAidesReno.usecase.ts
+++ src/usecase/mesAidesReno.usecase.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { KYCID } from 'src/domain/kyc/KYCID';
 import { DPE, TypeLogement } from '../domain/logement/logement';
 import { Scope, Utilisateur } from '../domain/utilisateur/utilisateur';
 import { CommuneRepository } from '../infrastructure/repository/commune/commune.repository';
 import { UtilisateurRepository } from '../infrastructure/repository/utilisateur/utilisateur.repository';
+import { QuestionKYCUsecase } from './questionKYC.usecase';
 
 const MES_AIDES_RENO_IFRAME_SIMULATION_URL =
   'https://mesaidesreno.beta.gouv.fr/simulation?iframe=true';
@@ -12,6 +14,7 @@ export class MesAidesRenoUsecase {
   constructor(
     private utilisateurRepository: UtilisateurRepository,
     private communeRepository: CommuneRepository,
+    private kycUsecase: QuestionKYCUsecase,
   ) {}
 
   async getIframeUrl(userId: string): Promise<string> {
@@ -28,6 +31,84 @@ export class MesAidesRenoUsecase {
     return `${MES_AIDES_RENO_IFRAME_SIMULATION_URL}&${params.toString()}`;
   }
 
+  async updateUtilisateurWith(
+    userId: string,
+    situation: Record<string, string>,
+  ): Promise<void> {
+    const utilisateur = await this.utilisateurRepository.getById(userId, [
+      Scope.kyc,
+      Scope.logement,
+    ]);
+
+    for (const [ruleName, value] of Object.entries(situation)) {
+      const jsValue = parsePublicodesValue(value);
+
+      switch (ruleName) {
+        case MesAidesRenoRuleNames.dpeActuel: {
+          if (jsValue === 1) {
+            utilisateur.logement.dpe = DPE.A;
+          } else if (jsValue === 2) {
+            utilisateur.logement.dpe = DPE.B;
+          } else if (jsValue === 3) {
+            utilisateur.logement.dpe = DPE.C;
+          } else if (jsValue === 4) {
+            utilisateur.logement.dpe = DPE.D;
+          } else if (jsValue === 5) {
+            utilisateur.logement.dpe = DPE.E;
+          } else if (jsValue === 6) {
+            utilisateur.logement.dpe = DPE.F;
+          } else if (jsValue === 7) {
+            utilisateur.logement.dpe = DPE.G;
+          }
+
+          // await this.utilisateurRepository.updateUtilisateur(utilisateur);
+          break;
+        }
+
+        case MesAidesRenoRuleNames.logementCodePostal: {
+          const codePostal = jsValue as string;
+          // const nomCommune =  situation[MesAidesRenoRuleNames.loge&];
+          break;
+        }
+
+        case MesAidesRenoRuleNames.logementProprietaire: {
+          if (jsValue === 'propriétaire' || jsValue === 'non propriétaire') {
+            const estProprietaire = jsValue === 'propriétaire';
+
+            await this.kycUsecase.updateResponseKYC_v2(
+              userId,
+              KYCID.KYC_proprietaire,
+              [
+                { code: 'oui', value: 'Oui', selected: estProprietaire },
+                { code: 'non', value: 'Non', selected: !estProprietaire },
+              ],
+            );
+          }
+          break;
+        }
+
+        // logementCommuneCodeInsee = 'logement . commune code insee',
+        // logementCommuneDepartement = 'logement . commune département',
+        // logementCommune = 'logement . commune',
+        // logementCommuneRegion = 'logement . commune région',
+        // logement = 'logement',
+        // logementPeriodeDeConstruction = 'logement . période de construction',
+        // logementProprietaireOccupant = 'logement . propriétaire occupant',
+        // logementProprietaire = 'vous . propriétaire . statut',
+        // logementResidencePrincipaleLocataire = 'logement . résidence principale locataire',
+        // logementResidencePrincipaleProprietaire = 'logement . résidence principale propriétaire',
+        // logementSurface = 'logement . surface',
+        // logementType = 'logement . type',
+        // menageCodeDepartement = 'ménage . code département',
+        // menageCodeEPCI = 'ménage . EPCI',
+        // menageCodeRegion = 'ménage . code région',
+        // menageCommune = 'ménage . commune',
+        // menagePersonnes = 'ménage . personnes',
+        // menageRevenu = 'ménage . revenu',
+      }
+    }
+  }
+
   /**
    * Returns the search parameters to be used to prefill the questions in the
    * iframe with the already known values from the user.
@@ -36,7 +117,7 @@ export class MesAidesRenoUsecase {
     const situation = {};
 
     if (utilisateur.logement?.proprietaire != null) {
-      situation['vous . propriétaire . statut'] =
+      situation[MesAidesRenoRuleNames.logementProprietaire] =
         // NOTE: missing the case 'acquéreur'
         utilisateur.logement.proprietaire
           ? '"propriétaire"'
@@ -45,13 +126,16 @@ export class MesAidesRenoUsecase {
       // NOTE: we assume that in the context of J'agis, the user is considered
       // as 'occupant' if he is 'proprietaire' and 'locataire'.
       if (utilisateur.logement.proprietaire) {
-        situation['logement . propriétaire occupant'] = 'oui';
-        situation['logement . résidence principale propriétaire'] = 'oui';
+        situation[MesAidesRenoRuleNames.logementProprietaireOccupant] = 'oui';
+        situation[
+          MesAidesRenoRuleNames.logementResidencePrincipaleProprietaire
+        ] = 'oui';
       }
     }
 
     if (utilisateur.logement?.plus_de_15_ans) {
-      situation['logement . période de construction'] = '"au moins 15 ans"';
+      situation[MesAidesRenoRuleNames.logementPeriodeDeConstruction] =
+        '"au moins 15 ans"';
     }
 
     if (
@@ -60,45 +144,47 @@ export class MesAidesRenoUsecase {
     ) {
       switch (utilisateur.logement.dpe) {
         case DPE.A:
-          situation['DPE . actuel'] = '1';
+          situation[MesAidesRenoRuleNames.dpeActuel] = '1';
           break;
         case DPE.B:
-          situation['DPE . actuel'] = '2';
+          situation[MesAidesRenoRuleNames.dpeActuel] = '2';
           break;
         case DPE.C:
-          situation['DPE . actuel'] = '3';
+          situation[MesAidesRenoRuleNames.dpeActuel] = '3';
           break;
         case DPE.D:
-          situation['DPE . actuel'] = '4';
+          situation[MesAidesRenoRuleNames.dpeActuel] = '4';
           break;
         case DPE.E:
-          situation['DPE . actuel'] = '5';
+          situation[MesAidesRenoRuleNames.dpeActuel] = '5';
           break;
         case DPE.F:
-          situation['DPE . actuel'] = '6';
+          situation[MesAidesRenoRuleNames.dpeActuel] = '6';
           break;
         case DPE.G:
-          situation['DPE . actuel'] = '7';
+          situation[MesAidesRenoRuleNames.dpeActuel] = '7';
           break;
       }
     }
 
     const nb_personnes = utilisateur.getNombrePersonnesDansLogement();
     if (nb_personnes > 0) {
-      situation['ménage . personnes'] = nb_personnes.toString();
+      situation[MesAidesRenoRuleNames.menagePersonnes] =
+        nb_personnes.toString();
     }
 
     if (utilisateur.revenu_fiscal != null) {
-      situation['ménage . revenu'] = utilisateur.revenu_fiscal.toString();
+      situation[MesAidesRenoRuleNames.menageRevenu] =
+        utilisateur.revenu_fiscal.toString();
     }
 
     if (utilisateur.logement?.type) {
       switch (utilisateur.logement.type) {
         case TypeLogement.maison:
-          situation['logement . type'] = '"maison"';
+          situation[MesAidesRenoRuleNames.logementType] = '"maison"';
           break;
         case TypeLogement.appartement:
-          situation['logement . type'] = '"appartement"';
+          situation[MesAidesRenoRuleNames.logementType] = '"appartement"';
           break;
       }
     }
@@ -109,19 +195,61 @@ export class MesAidesRenoUsecase {
         utilisateur.logement.commune,
       );
       const commune = this.communeRepository.getCommuneByCodeINSEE(code_insee);
+      const epci = this.communeRepository.getEPCIByCommuneCodeINSEE(code_insee);
+
       if (commune) {
-        situation['ménage . commune'] = `"${commune.code}"`;
-        situation['ménage . code région'] = `"${commune.region}"`;
-        situation['ménage . code département'] = `"${commune.departement}"`;
-        situation['ménage . EPCI'] = `"${commune.code}"`;
+        situation[MesAidesRenoRuleNames.menageCommune] = `"${commune.code}"`;
+        situation[
+          MesAidesRenoRuleNames.menageCodeRegion
+        ] = `"${commune.region}"`;
+        situation[
+          MesAidesRenoRuleNames.menageCodeDepartement
+        ] = `"${commune.departement}"`;
+        situation[MesAidesRenoRuleNames.menageCodeEPCI] = `"${epci.code}"`;
         // NOTE: we assume that in the context of J'agis, the user is considered
         // as 'occupant' if he is 'proprietaire' and 'locataire'.
-        situation['logement . commune'] = commune.code;
+        situation[
+          MesAidesRenoRuleNames.logementCommuneCodeInsee
+        ] = `"${commune.code}"`;
+        situation[
+          MesAidesRenoRuleNames.logementCommuneDepartement
+        ] = `"${commune.departement}"`;
+        situation[
+          MesAidesRenoRuleNames.logementCommuneRegion
+        ] = `"${commune.region}"`;
+        situation[
+          MesAidesRenoRuleNames.logementCommuneNom
+        ] = `"${commune.nom}"`;
+        situation[
+          MesAidesRenoRuleNames.logementCodePostal
+        ] = `"${utilisateur.logement.code_postal}"`;
       }
     }
 
     return getParamsFromSituation(situation);
   }
+}
+
+enum MesAidesRenoRuleNames {
+  dpeActuel = 'DPE . actuel',
+  logementCodePostal = 'logement . code postal',
+  logementCommuneCodeInsee = 'logement . commune',
+  logementCommuneDepartement = 'logement . commune département',
+  logementCommuneNom = 'logement . commune . nom',
+  logementCommuneRegion = 'logement . commune région',
+  logementPeriodeDeConstruction = 'logement . période de construction',
+  logementProprietaireOccupant = 'logement . propriétaire occupant',
+  logementProprietaire = 'vous . propriétaire . statut',
+  logementResidencePrincipaleLocataire = 'logement . résidence principale locataire',
+  logementResidencePrincipaleProprietaire = 'logement . résidence principale propriétaire',
+  logementSurface = 'logement . surface',
+  logementType = 'logement . type',
+  menageCodeDepartement = 'ménage . code département',
+  menageCodeEPCI = 'ménage . EPCI',
+  menageCodeRegion = 'ménage . code région',
+  menageCommune = 'ménage . commune',
+  menagePersonnes = 'ménage . personnes',
+  menageRevenu = 'ménage . revenu',
 }
 
 function getParamsFromSituation(
@@ -139,3 +267,30 @@ function getParamsFromSituation(
 // Copied from https://github.com/betagouv/reno/blob/bea6cc74bd776a477141b77d78d37c330f7191f0/components/publicodes/situationUtils.ts#L3
 // NOTE: why not using the publicodes utils.encodeRuleName function?
 export const encodeDottedName = (decoded) => decoded.replace(/\s\.\s/g, '.');
+
+function parsePublicodesValue(
+  value: string | null,
+): string | number | boolean | null {
+  if (!value) {
+    return null;
+  }
+  if (value === 'oui') {
+    return true;
+  }
+  if (value === 'non') {
+    return false;
+  }
+  if (value === 'null') {
+    return null;
+  }
+  if (
+    (value.startsWith('"') && value.endsWith('"')) ||
+    (value.startsWith("'") && value.endsWith("'"))
+  ) {
+    return value.slice(1, -1);
+  }
+  if (!isNaN(Number(value))) {
+    return Number(value);
+  }
+  return value;
+}
--- test/integration/usecase/mesAidesReno.usecase.int-spec.ts
+++ test/integration/usecase/mesAidesReno.usecase.int-spec.ts
@@ -1,15 +1,31 @@
-import { TypeLogement } from '../../../src/domain/logement/logement';
+import { KYCID } from '../../../src/domain/kyc/KYCID';
+import { TypeReponseQuestionKYC } from '../../../src/domain/kyc/questionKYC';
+import {
+  Chauffage,
+  DPE,
+  Superficie,
+  TypeLogement,
+} from '../../../src/domain/logement/logement';
+import { Scope } from '../../../src/domain/utilisateur/utilisateur';
+import { Personnalisator } from '../../../src/infrastructure/personnalisation/personnalisator';
 import { CommuneRepository } from '../../../src/infrastructure/repository/commune/commune.repository';
+import { KycRepository } from '../../../src/infrastructure/repository/kyc.repository';
 import { UtilisateurRepository } from '../../../src/infrastructure/repository/utilisateur/utilisateur.repository';
 import { MesAidesRenoUsecase } from '../../../src/usecase/mesAidesReno.usecase';
+import { QuestionKYCUsecase } from '../../../src/usecase/questionKYC.usecase';
 import { DB, TestUtil } from '../../TestUtil';
 
-describe('MesAidesRenoRepository', () => {
+describe('Mes Aides Réno', () => {
   const utilisateurRepository = new UtilisateurRepository(TestUtil.prisma);
   const communeRepository = new CommuneRepository(TestUtil.prisma);
+  const kycRepository = new KycRepository(TestUtil.prisma);
   const usecase = new MesAidesRenoUsecase(
     utilisateurRepository,
     communeRepository,
+    new QuestionKYCUsecase(
+      utilisateurRepository,
+      new Personnalisator(communeRepository),
+    ),
   );
 
   beforeAll(async () => {
@@ -22,6 +38,71 @@ describe('MesAidesRenoRepository', () => {
 
   afterAll(async () => {
     await TestUtil.appclose();
+  });
+
+  describe('updateUtilisateurWith', () => {
+    test.only("propriétaire d'une maison principale à Toulouse", async () => {
+      await TestUtil.create(DB.utilisateur, {
+        logement: {
+          version: 0,
+          superficie: Superficie.superficie_150,
+          type: TypeLogement.appartement,
+          code_postal: '91120',
+          chauffage: Chauffage.bois,
+          commune: 'PALAISEAU',
+          dpe: DPE.B,
+          nombre_adultes: 2,
+          nombre_enfants: 2,
+          plus_de_15_ans: false,
+          proprietaire: false,
+        },
+      });
+      await TestUtil.create(DB.kYC, {
+        id_cms: 1,
+        code: KYCID.KYC_proprietaire,
+        type: TypeReponseQuestionKYC.choix_unique,
+        reponses: [
+          { code: 'oui', label: 'Oui' },
+          { code: 'non', label: 'Non' },
+        ],
+      });
+      await kycRepository.loadCache();
+
+      await usecase.updateUtilisateurWith('utilisateur-id', {
+        'vous . propriétaire . statut': '"propriétaire"',
+        'logement . propriétaire occupant': 'oui',
+        'logement . résidence principale propriétaire': 'oui',
+        'logement . type': '"maison"',
+        'logement . surface': '30',
+        'logement . période de construction': '"de 2 à 10 ans"',
+        'ménage . personnes': '2',
+        'ménage . code région': '"76"',
+        'ménage . code département': '"31"',
+        'ménage . EPCI': '"243100518"',
+        'ménage . commune': '"31555"',
+        'ménage . commune . nom': '"Toulouse"',
+        'taxe foncière . commune . éligible . ménage': 'non',
+        'logement . commune . denormandie': 'non',
+        'ménage . revenu': '32197',
+        'DPE . actuel': '3',
+      });
+
+      const utilisateur = await utilisateurRepository.getById(
+        'utilisateur-id',
+        [Scope.logement, Scope.kyc],
+      );
+      console.log('utilisateur:', {
+        id: utilisateur.id,
+        email: utilisateur.email,
+      });
+      console.log('logement:', utilisateur.logement);
+      console.log('revenu_fiscal:', utilisateur.revenu_fiscal);
+      console.log('code_commune:', utilisateur.code_commune);
+      console.log('nbPerssones:', utilisateur.getNombrePersonnesDansLogement());
+
+      expect(utilisateur.logement.dpe).toEqual(DPE.C);
+      expect(utilisateur.logement.proprietaire).toBeTruthy();
+    });
   });
 
   describe('getIframeUrl', () => {
