3cb8fc37d589d98c818008d2b21034a210497973
Emile Rolley <emile.rolley@tuta.io>
2025-05-02 17:53:51 +0200

    feat(mes_aides_reno): ajout de l'iframe pour skipper les questions + refactor de l'endpoint pour récupérer l'iframe

--- test/integration/usecase/mesAidesReno.usecase.int-spec.ts
+++ test/integration/usecase/mesAidesReno.usecase.int-spec.ts
@@ -210,7 +210,10 @@ describe('Mes Aides Réno', () => {
   describe('getIframeUrl', () => {
     test("should return the url without params if the user doesn't exist", async () => {
       const result = await usecase.getIframeUrl('non_existant_id');
-      expect(result).toBe(
+      expect(result.iframe_url).toBe(
+        'https://mesaidesreno.beta.gouv.fr/simulation?iframe=true',
+      );
+      expect(result.iframe_url_deja_faite).toBe(
         'https://mesaidesreno.beta.gouv.fr/simulation?iframe=true',
       );
     });
@@ -241,8 +244,11 @@ describe('Mes Aides Réno', () => {
       });
 
       const result = await usecase.getIframeUrl('utilisateur-id');
-      expect(result).toBe(
+      expect(result.iframe_url).toBe(
         'https://mesaidesreno.beta.gouv.fr/simulation?iframe=true&DPE.actuel=2&logement.p%C3%A9riode+de+construction=%22au+moins+15+ans%22&logement.propri%C3%A9taire+occupant=oui&vous.propri%C3%A9taire.statut=%22propri%C3%A9taire%22&logement.r%C3%A9sidence+principale+propri%C3%A9taire=oui&logement.surface=125&logement.type=%22appartement%22&m%C3%A9nage.personnes=2&m%C3%A9nage.revenu=20000&m%C3%A9nage.commune=%2231555%22&m%C3%A9nage.code+r%C3%A9gion=%2276%22&m%C3%A9nage.code+d%C3%A9partement=%2231%22&m%C3%A9nage.EPCI=%22243100518%22&logement.commune=%2231555%22&logement.commune+d%C3%A9partement=%2231%22&logement.commune+r%C3%A9gion=%2276%22&logement.commune.nom=%22Toulouse%22&logement.code+postal=%2231500%22',
+      );
+      expect(result.iframe_url_deja_faite).toBe(
+        'https://mesaidesreno.beta.gouv.fr/simulation?iframe=true&DPE.actuel=2*&logement.p%C3%A9riode+de+construction=%22au+moins+15+ans%22*&logement.propri%C3%A9taire+occupant=oui*&vous.propri%C3%A9taire.statut=%22propri%C3%A9taire%22*&logement.r%C3%A9sidence+principale+propri%C3%A9taire=oui*&logement.surface=125*&logement.type=%22appartement%22*&m%C3%A9nage.personnes=2*&m%C3%A9nage.revenu=20000*&m%C3%A9nage.commune=%2231555%22*&m%C3%A9nage.code+r%C3%A9gion=%2276%22*&m%C3%A9nage.code+d%C3%A9partement=%2231%22*&m%C3%A9nage.EPCI=%22243100518%22*&logement.commune=%2231555%22*&logement.commune+d%C3%A9partement=%2231%22*&logement.commune+r%C3%A9gion=%2276%22*&logement.commune.nom=%22Toulouse%22*&logement.code+postal=%2231500%22*',
       );
     });
 
@@ -286,8 +292,11 @@ describe('Mes Aides Réno', () => {
       utilisateur.kyc_history.updateQuestion(kyc);
 
       const result = await usecase.getIframeUrl('utilisateur-id');
-      expect(result).toBe(
+      expect(result.iframe_url).toBe(
         'https://mesaidesreno.beta.gouv.fr/simulation?iframe=true&DPE.actuel=2&logement.p%C3%A9riode+de+construction=%22au+moins+15+ans%22&logement.propri%C3%A9taire+occupant=oui&vous.propri%C3%A9taire.statut=%22propri%C3%A9taire%22&logement.r%C3%A9sidence+principale+propri%C3%A9taire=oui&logement.surface=50&logement.type=%22appartement%22&m%C3%A9nage.personnes=2&m%C3%A9nage.revenu=20000&m%C3%A9nage.commune=%2231555%22&m%C3%A9nage.code+r%C3%A9gion=%2276%22&m%C3%A9nage.code+d%C3%A9partement=%2231%22&m%C3%A9nage.EPCI=%22243100518%22&logement.commune=%2231555%22&logement.commune+d%C3%A9partement=%2231%22&logement.commune+r%C3%A9gion=%2276%22&logement.commune.nom=%22Toulouse%22&logement.code+postal=%2231500%22',
+      );
+      expect(result.iframe_url_deja_faite).toBe(
+        'https://mesaidesreno.beta.gouv.fr/simulation?iframe=true&DPE.actuel=2*&logement.p%C3%A9riode+de+construction=%22au+moins+15+ans%22*&logement.propri%C3%A9taire+occupant=oui*&vous.propri%C3%A9taire.statut=%22propri%C3%A9taire%22*&logement.r%C3%A9sidence+principale+propri%C3%A9taire=oui*&logement.surface=50*&logement.type=%22appartement%22*&m%C3%A9nage.personnes=2*&m%C3%A9nage.revenu=20000*&m%C3%A9nage.commune=%2231555%22*&m%C3%A9nage.code+r%C3%A9gion=%2276%22*&m%C3%A9nage.code+d%C3%A9partement=%2231%22*&m%C3%A9nage.EPCI=%22243100518%22*&logement.commune=%2231555%22*&logement.commune+d%C3%A9partement=%2231%22*&logement.commune+r%C3%A9gion=%2276%22*&logement.commune.nom=%22Toulouse%22*&logement.code+postal=%2231500%22*',
       );
     });
 
@@ -317,8 +326,11 @@ describe('Mes Aides Réno', () => {
       });
 
       const result = await usecase.getIframeUrl('utilisateur-id');
-      expect(result).toBe(
+      expect(result.iframe_url).toBe(
         'https://mesaidesreno.beta.gouv.fr/simulation?iframe=true&DPE.actuel=2&logement.propri%C3%A9taire+occupant=oui&vous.propri%C3%A9taire.statut=%22propri%C3%A9taire%22&logement.r%C3%A9sidence+principale+propri%C3%A9taire=oui&m%C3%A9nage.personnes=2&m%C3%A9nage.revenu=20000',
+      );
+      expect(result.iframe_url_deja_faite).toBe(
+        'https://mesaidesreno.beta.gouv.fr/simulation?iframe=true&DPE.actuel=2*&logement.propri%C3%A9taire+occupant=oui*&vous.propri%C3%A9taire.statut=%22propri%C3%A9taire%22*&logement.r%C3%A9sidence+principale+propri%C3%A9taire=oui*&m%C3%A9nage.personnes=2*&m%C3%A9nage.revenu=20000*',
       );
     });
   });
