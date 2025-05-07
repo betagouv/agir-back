55ca0082d6043a82a2c1f93bb4e778ed38a4c93b
Wojciech WOJCIK <wojciech.wojcik@gmail.com>
2025-05-07 16:15:01 +0200

    WIP reco V2

---
+++ src/domain/scoring/system_v2/Tag_v2.ts
@@ -0,0 +1,7 @@
+export enum Tag_v2 {
+  est_proprietaire = 'est_proprietaire',
+  est_locataire = 'est_locataire',
+  mange_de_la_viande = 'mange_de_la_viande',
+  prends_l_avion = 'prends_l_avion',
+  a_une_voiture = 'a_une_voiture',
+}
