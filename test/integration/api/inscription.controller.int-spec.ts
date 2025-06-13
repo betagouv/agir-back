2cc5a8ecfdf5e7f644c2db268fda17a4a3b06e7d
Wojciech WOJCIK <wojciech.wojcik@gmail.com>
2025-06-13 14:06:36 +0200

    up version user => 21

--- test/integration/api/inscription.controller.int-spec.ts
+++ test/integration/api/inscription.controller.int-spec.ts
@@ -107,7 +107,7 @@ suite à un problème technique, vous ne pouvez pas vous inscrire au service J'a
       where: { email: 'w@w.com' },
     });
     expect(response.status).toBe(201);
-    expect(user.version).toEqual(20);
+    expect(user.version).toEqual(21);
   });
 
   it('POST /utilisateurs_v2 - bad password', async () => {
