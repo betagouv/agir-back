778e60001965eda950f04f3f7c70f21a6d6f3478
Wojciech WOJCIK <wojciech.wojcik@gmail.com>
2025-07-07 22:56:49 +0200

    suppression table Statistique et DefiStatistique

---
+++ src/infrastructure/prisma/migrations/20250707205224_suppr_old_stats/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the `DefiStatistique` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Statistique` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "DefiStatistique";
+
+-- DropTable
+DROP TABLE "Statistique";
