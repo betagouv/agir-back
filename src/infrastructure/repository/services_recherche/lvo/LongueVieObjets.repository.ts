1177bb8f2341dfd32860e9b5e44f690f3550eb40
Wojciech WOJCIK <wojciech.wojcik@gmail.com>
2025-08-27 13:51:54 +0200

    fix mapping LVAO categorie absente #2

--- src/infrastructure/repository/services_recherche/lvo/LongueVieObjets.repository.ts
+++ src/infrastructure/repository/services_recherche/lvo/LongueVieObjets.repository.ts
@@ -161,7 +161,7 @@ export class LongueVieObjetsRepository implements FinderInterface {
       limit: filtre.nombre_max_resultats ? filtre.nombre_max_resultats : 10,
     };
 
-    if (categorie) {
+    if (categorie && categorie !== 'undefined') {
       params['actions'] = categorie;
     }
 
